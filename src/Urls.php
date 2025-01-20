99544ba7d0a974dd9c2c5b6acc2aeac1ff7e2550
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-01-20 12:30:44 +0000

    Add validation to unparse_url function to ensure input is array

--- src/Urls.php
+++ src/Urls.php
@@ -1,27 +1,37 @@
-<?php declare(strict_types=1);
+<?php 
 
 namespace Missing;
+
+use InvalidArgumentException;
 
 class Urls {
 
     public static function unparse_url(array $parsed): string {
-        $pass      = $parsed['pass'] ?? null;
-        $user      = $parsed['user'] ?? null;
+        if(!is_array($parsed)) {
+            throw new InvalidArgumentException("Input must be an array returned by parse_url");
+        };
+
+        $scheme = $parsed['scheme'] ?? null;
+        $host = $parsed['host'] ?? null;
+        $port = $parsed['port'] ?? null;
+        $user = $parsed['user'] ?? null;
+        $pass = $parsed['pass'] ?? null;
+        $path = $parsed['path'] ?? '';
+        $query = $parsed['query'] ?? null;
+        $fragment = $parsed['fragment'] ?? null;
+        
         $userinfo  = $pass !== null ? "$user:$pass" : $user;
-        $port      = $parsed['port'] ?? 0;
-        $scheme    = $parsed['scheme'] ?? null;
-        $query     = $parsed['query'] ?? null;
-        $fragment  = $parsed['fragment'] ?? "";
         $authority = (
             ($userinfo !== null ? "$userinfo@" : "") .
             ($parsed['host'] ?? "") .
             ($port ? ":$port" : "")
         );
         return (
-            (\strlen($authority) > 0 ? "//$authority" : "") .
-            ($parsed['path'] ?? "") .
-            (\strlen($query ?? '') > 0 ? "?$query" : "") .
-            (\strlen($fragment) > 0 ? "#$fragment" : "")
+            ($scheme !== null ? "$scheme://" : "") .
+            ($authority !== null ? "//$authority" : "") .
+            ($path !== null ?? "") .
+            ($query !== null ? "$query" : "") .
+            ($fragment !== null ? "#$fragment" : "")
         );
     }
 
