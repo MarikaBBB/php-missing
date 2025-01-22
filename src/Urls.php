c7170f6e9661067c68df3508c0e6e4c38ea6bf2f
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-01-22 12:00:23 +0000

    Add scheme and host to unparse_url

--- src/Urls.php
+++ src/Urls.php
@@ -6,11 +6,11 @@ class Urls
 {
     public static function unparse_url(array $parsed): string
     {
-        // $scheme = isset($parsed['scheme']) ? $parsed['scheme'] . "://" : "";
-        // $host = isset($parsed['host']) ? $parsed['host'] : "";
+        $scheme = isset($parsed['scheme']) ? $parsed['scheme'] . "://" : "";
+        $host = isset($parsed['host']) ? $parsed['host'] : "";
         $path = $parsed['path'] ?? '';
 
-        return $path;
+        return "$scheme$host$path";
 
     }
 }
