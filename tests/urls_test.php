92f936f361de0a83a78c96d42fd6e1097e877cd4
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-01-22 12:53:46 +0000

    Refactor PHPUnit test class for unparse_url

--- tests/urls_test.php
+++ tests/urls_test.php
@@ -7,114 +7,27 @@ use Missing\Urls;
 
 class Urls_test extends TestCase
 {
-    // public function test_unparse_url()
-    // {
-    //     $url = '';
-    //     $parsed = parse_url($url);
-    //     $rebuildUrl = Urls::unparse_url($parsed);
-    //     $parsedRebuilt = parse_url($rebuildUrl);
-
-
-    //    $this->assertEquals($parsed, $parsedRebuilt, 'Failed  to reconstruct empty string URL');
-    // }
-
-    public function testUnparseUrlString()
+    public function test_unparse_url()
     {
-        $url = 'foo';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
+        $urls = [
+            '',
+            'foo',
+            'http://www.google.com/',
+            'http://www.google.com:8080/',
+            'http://u:p@foo:1/path/path?q#frag',
+            'http://u:p@foo:1/path/path?#',
+            'ssh://root@host',
+            '://:@:1/?#',
+            'http://:@foo:1/path/path?#'
+        ];
 
+        foreach ($urls as $url) {
+            $parsed = parse_url($url);
+            $rebuildUrl = Urls::unparse_url($parsed);
+            $parsedRebuilt = parse_url($rebuildUrl);
+            
+            $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct URL: '{$url}'");
 
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct simple string URL: '{$url}'");
-    }
-
-    public function testUnparseSimpleUrl()
-    {
-        $url = 'http://www.google.com/';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct simple URL: '{$url}'");
-    }
-
-    public function testUnparseHostUrl()
-    {
-        $url = 'http://www.google.com:8080/';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct URL host and port: '{$url}'");
-    }
-
-    public function testUnparseFullUrl()
-    {
-        $url = 'http://u:p@foo:1/path/path?q#frag';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct full URL: '{$url}'");
-    }
-
-    public function testUnparseUrlIncomplete()
-    {
-        $url = 'http://u:p@foo:1/path/path?#';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct incomplete URL: '{$url}'");
-    }
-
-    public function testUnparseUrlSsh()
-    {
-        $url = 'ssh://root@host';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct SSH URL: '{$url}'");
-    }
-
-    public function testUnparseMalformedUrl()
-    {
-        $url = '://:@:1/?#';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct malformed URL: '{$url}'");
-    }
-
-    public function testUnparseUrlWithEmptyUserAndPass()
-    {
-        $url = 'http://:@foo:1/path/path?#';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct URL with empty user and password: '{$url}'");
-    }
-
-
-    public function testUnparseUrlWithEmptyUser()
-    {
-        $url = 'http://:@foo:1/path/path?#';
-        $parsed = parse_url($url);
-        $rebuildUrl = Urls::unparse_url($parsed);
-        $parsedRebuilt = parse_url($rebuildUrl);
-
-
-       $this->assertEquals($parsed, $parsedRebuilt, "Failed  to reconstruct URL with empty user: '{$url}'");
+        }
     }
 }
