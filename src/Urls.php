ebd8d5b540d57c0d421ec5d46b3ad13376761dd5
Marika Bertelli <marikabertelli@MX25FH6GHJ.local>
2025-01-22 10:54:08 +0000

    Implement unparse_url to handle path only URL

--- src/Urls.php
+++ src/Urls.php
@@ -6,6 +6,10 @@ class Urls
 {
     public static function unparse_url(array $parsed): string
     {
-        return '';
+        // $scheme = isset($parsed['scheme']) ? $parsed['scheme'] . "://" : "";
+        // $host = isset($parsed['host']) ? $parsed['host'] : "";
+
+        return $parsed['path'] ?? '';
+
     }
 }
